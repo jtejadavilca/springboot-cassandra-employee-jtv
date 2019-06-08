bce422a3f4c92547e9e0a6704436534aaffbef57
Jose Tejada <jtejadavilca@gmail.com>
2019-06-08 15:44:46 -0500

    Proyecto inicial con SpringBoot y Cassandra que implementa CRUD de Employee para curso JavaExpert de CJAVA

---
+++ src/main/java/pe/com/pruebas/cassandrajtv/CassandrajtvApplication.java
@@ -0,0 +1,13 @@
+package pe.com.pruebas.cassandrajtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandrajtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandrajtvApplication.class, args);
+	}
+
+}
